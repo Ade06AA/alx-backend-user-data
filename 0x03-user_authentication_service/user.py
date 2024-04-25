1e083299499e5f8c9af6765db1032f810223872a
OadeO6 <abdurrahman.adenowo06@gmail.com>
2024-04-25 15:50:19 -0700

    update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 user ORM module
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String<F11>
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
@@ -16,7 +16,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True,
                 unique=True, nullable=False)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
